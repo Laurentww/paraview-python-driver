3c72ca896984d61450a55c90040f5814d72e2868
Laurentww <laurentwilkens@gmail.com>
2022-01-03 23:47:00 +0100

    Remove OS-dependent path separators

--- install.py
+++ install.py
@@ -10,6 +10,8 @@ from subprocess import getoutput
 from sys import version_info as py_ver, exec_prefix
 from textwrap import dedent
 
+HOME = expanduser('~')
+
 parser = ArgumentParser(
     description='ParaView install script',
     prog=abspath(__file__),
@@ -22,39 +24,41 @@ parser = ArgumentParser(
 parser.add_argument(
     '--prefix', type=str,
     help="Path to folder where ParaView modules and dependencies will be installed",
-    default='~/local'
+    default=join(HOME, 'local')
 )
 args = parser.parse_args()
 
+install_prefix = expanduser(args.prefix)
+
 # Install ParaView to system
 install_script = join(dirname(realpath(__file__)), 'install.sh')
-system(f"chmod +x {install_script} && bash {install_script} --prefix {args.prefix}")
+system(f"chmod +x {install_script} && bash {install_script} --prefix {install_prefix}")
 
 # Find source location of packages and library files
 find_folder = lambda w_dir, s: getoutput(f"find {w_dir} -maxdepth 1 -type d -name {s}")
 python_src = lambda w_dir: find_folder(w_dir, 'python*')
 
 paraview_locations = [
-    find_folder("~/paraview/build", 'lib*'),
-    join(args.prefix, "lib"),
-    join(args.prefix, "lib64")
+    find_folder(join(HOME, "paraview", "build"), 'lib*'),
+    join(install_prefix, "lib"),
+    join(install_prefix, "lib64")
 ]
-src_lib_dir, src_python_dir = None, None
-for location in [expanduser(loc) for loc in paraview_locations]:
-    src_lib_dir = location
-    src_python_dir = python_src(location)
-    if isfile(f"{src_python_dir}/site-packages/vtk.py"):
+src_python_dir, src_lib_dir = None, None
+for src_lib_dir in paraview_locations:
+    src_python_dir = python_src(src_lib_dir)
+    if isfile(join(src_python_dir, "site-packages", "vtk.py")):
         break
 
-dest_lib = f"{exec_prefix}/lib"
+dest_lib = join(exec_prefix, "lib")
+dest_site = join(dest_lib, f"python{py_ver.major}.{py_ver.minor}", "site-packages")
 
 # Copy ParaView python packages and library files into current python environment
-for file in scandir(f"{src_python_dir}/site-packages/"):  # type: DirEntry
-    dest = f"{dest_lib}/python{py_ver.major}.{py_ver.minor}/site-packages/{file.name}"
+for file in scandir(join(src_python_dir, "site-packages")):  # type: DirEntry
+    dest_dir = join(dest_site, file.name)
     if file.is_file():
-        copyfile(file.path, dest)
+        copyfile(file.path, dest_dir)
     else:
-        copytree(file.path, dest, dirs_exist_ok=True)
+        copytree(file.path, dest_dir, dirs_exist_ok=True)
 
 for file in [
     "RemotingCore",
@@ -69,8 +73,8 @@ for file in [
 ]:
     filename = f"libvtk{file}-pv5.10.so.1"
 
-    # print(f'Copying `{f"{src_lib_dir}/{filename}"}` to {exec_prefix}/lib/ ..')
-    copyfile(f"{src_lib_dir}/{filename}", f"{exec_prefix}/lib/{filename}")
+    # print(f'Copying {join(src_lib_dir, filename)} to {join(exec_prefix, "lib", "")}')
+    copyfile(join(src_lib_dir, filename), join(exec_prefix, "lib", filename))
 
 print(
     ".. Done installing ParaView Python packages and library files "
